fd044b2308b8917d629a52057a5512ebb3b8118b
johir-rayhan <johir.rayhan01@gmail.com>
2018-09-19 18:37:09 +0600

    Create test.js

---
+++ app/test.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Tornadoes it's Website!</h1>
+    <Link to="/register" className="new-account">Don't have an account?</Link>
+  </div>
+)
+
+export default Home
