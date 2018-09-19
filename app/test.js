160129f006eb732553c35de2311cbe7628b4d796
johir-rayhan <johir.rayhan01@gmail.com>
2018-09-19 18:37:53 +0600

    Update test.js

--- app/test.js
+++ app/test.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => (
   <div>
-    <h1>Welcome to the Tornadoes it's Website!</h1>
+    <h1>{"Welcome to the Tornadoes it's Website!"}</h1>
     <Link to="/register" className="new-account">Don't have an account?</Link>
   </div>
 )
