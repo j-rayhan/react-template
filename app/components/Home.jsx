5bf259c5bfb201a99b0fde0cd1e7bb6586716a93
johir-rayhan <johir.rayhan01@gmail.com>
2018-09-19 18:02:58 +0600

    Update Home.jsx

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Home = () => (
   <div>
     <h1>Welcome to the Tornadoes it's Website!</h1>
+    <p className="mt-4 small">Enter your email address and we'll send you the instructions for resetting the password.</p>
   </div>
 )
 
